f0b3ae0e1d755ae3e8d4f7832ee7a2d774d232be
NVD HEMANTH <91491352+hemanthnvd@users.noreply.github.com>
2024-02-13 21:55:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 const app = require('./app');
 const { sequelize } = require('./db');
-const mysql = require('mysql2/promise');
+// const mysql = require('mysql2/promise');
 const user = require('./models/user');
 const port = process.env.PORT || 8080;
 
 app.listen(port,()=>{
-  mysql.createConnection({
-    user:'root',        
-    password : '123'
-  }).then((connection) => {
-    connection.query('CREATE DATABASE IF NOT EXISTS assignment01;').then(() => {
-      sequelize.sync().then((result)=>{
-        // console.log(result);
-      }).catch((err)=>{
-        console.log(err);
-      });
-    })
-  })  
+  sequelize.sync().then((result)=>{
+      // console.log(result);
+    }).catch((err)=>{
+      console.log(err);
+  });
   console.log("Server running on port " + port)
-});
+})
 
 
 
