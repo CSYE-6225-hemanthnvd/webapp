11f1158d3da4f7e6b2632cdf5908ca462645042c
hemanthnvd <hemanthnvd@gmail.com>
2024-04-03 16:11:32 -0400

    Added user already verified validation

--- controllers/v1UserVerify.js
+++ controllers/v1UserVerify.js
@@ -1,33 +1,36 @@
 const logger = require("../logger");
 const user = require('../models/user');
 
-const v1UserVerify = async (req,res,next)=>{
-  const currentUser = await user.findOne({where:{id:req.query.id}});
-  if(!currentUser){
+const v1UserVerify = async (req, res, next) => {
+  const currentUser = await user.findOne({ where: { id: req.query.id } });
+  if (!currentUser) {
     logger.warn({
       message: "Someone tried to verify with wrong userid",
       log_type: "User Verification",
     });
-    return res.setHeader("Cache-Control", "no-cache").status(400).json({message:'Invalid user'}).end();
-  }else{
+    return res.setHeader("Cache-Control", "no-cache").status(400).json({ message: 'Invalid user' }).end();
+  } else {
+    if (currentUser.is_verified) {
+      return res.setHeader("Cache-Control", "no-cache").status(200).json({ message: 'Your email address has already been verified' }).end();
+    }
     currentUser.link_clicked = Date.now();
     currentUser.save();
-    if((currentUser.link_clicked - currentUser.email_sent) < 120000){
+    if ((currentUser.link_clicked - currentUser.email_sent) < 120000) {
       currentUser.is_verified = true;
       currentUser.save();
       logger.warn({
         message: "User verified successfully",
         log_type: "User Verification",
       });
-      return res.setHeader("Cache-Control", "no-cache").status(200).json({message:'Email verified successfully'}).end();
-    }else{
+      return res.setHeader("Cache-Control", "no-cache").status(200).json({ message: 'Email verified successfully' }).end();
+    } else {
       logger.warn({
         message: "User tried to verify with expired link",
         log_type: "User Verification",
       });
-      return res.setHeader("Cache-Control", "no-cache").status(410).json({message:'Verification link expired'}).end();
+      return res.setHeader("Cache-Control", "no-cache").status(410).json({ message: 'Verification link expired' }).end();
     }
   }
 }
 
-module.exports=v1UserVerify;
+module.exports = v1UserVerify;
